5190c0e822cb596ba852a55f6b218796c9894c89
AlanFRL <tengounsupercell@gmail.com>
2024-04-15 23:44:38 -0400

    Roles y permisos actualizados

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -20,35 +20,55 @@ class RoleSeeder extends Seeder
         $role4 = Role::create(['name' => 'Paciente']);
 
         // Routes Admin.php
-        Permission::create(['name' => 'home'])->syncRoles([$role1,$role2,$role3]);
-        //TipoSeguro
-        Permission::create(['name' => 'tiposeguro.index'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'tiposeguro.create'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'tiposeguro.store'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'tiposeguro.edit'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'tiposeguro.update'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'tiposeguro.destroy'])->syncRoles([$role1,$role2]);
-        
-        //TipoAnalisis   
-        Permission::create(['name' => 'tipoanalisis.index'])->syncRoles([$role1,$role3]);
-        Permission::create(['name' => 'tipoanalisis.create'])->syncRoles([$role1,$role3]);
-        Permission::create(['name' => 'tipoanalisis.store'])->syncRoles([$role1,$role3]);
-        Permission::create(['name' => 'tipoanalisis.edit'])->syncRoles([$role1,$role3]);
-        Permission::create(['name' => 'tipoanalisis.update'])->syncRoles([$role1,$role3]);
-        Permission::create(['name' => 'tipoanalisis.destroy'])->syncRoles([$role1,$role3]);
+        Permission::create(['name' => 'home'])->syncRoles([$role1, $role2, $role3]);
 
-        //Pacientes   
-        Permission::create(['name' => 'pacientes.index'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'pacientes.create'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'pacientes.store'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'pacientes.edit'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'pacientes.update'])->syncRoles([$role1,$role2]);
-        Permission::create(['name' => 'pacientes.destroy'])->syncRoles([$role1,$role2]);
+        //Paquete Usuarios
+        Permission::create(['name' => 'users.package'])->syncRoles([$role1]);
+
+        //Usuarios
+        Permission::create(['name' => 'users.index'])->syncRoles([$role1]);
 
         //Roles  
         Permission::create(['name' => 'roles.index'])->syncRoles([$role1]);
 
-        //Usuarios
-        Permission::create(['name' => 'users.index'])->syncRoles([$role1]);
+        //Paquete historiales
+        Permission::create(['name' => 'history.package'])->syncRoles([$role1, $role2]);
+
+        //Pacientes   
+        Permission::create(['name' => 'pacientes.index'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'pacientes.create'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'pacientes.store'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'pacientes.edit'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'pacientes.update'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'pacientes.destroy'])->syncRoles([$role1, $role2]);
+
+        //TipoSeguro
+        Permission::create(['name' => 'tiposeguro.index'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'tiposeguro.create'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'tiposeguro.store'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'tiposeguro.edit'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'tiposeguro.update'])->syncRoles([$role1, $role2]);
+        Permission::create(['name' => 'tiposeguro.destroy'])->syncRoles([$role1, $role2]);
+
+        //Paquete analisis
+        Permission::create(['name' => 'analysis.package'])->syncRoles([$role1, $role3]);
+
+        //TipoAnalisis   
+        Permission::create(['name' => 'tipoanalisis.index'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'tipoanalisis.create'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'tipoanalisis.store'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'tipoanalisis.edit'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'tipoanalisis.update'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'tipoanalisis.destroy'])->syncRoles([$role1, $role3]);
+
+
+
+
+
+
+
+
+
+        
     }
 }
