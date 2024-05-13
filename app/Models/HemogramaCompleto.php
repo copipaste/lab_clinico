38fffaef66c864e8eea3d30ef9955b7afde72414
Castedo_Daniel <pablodaniel280403@gmail.com>
2024-05-13 15:42:16 -0400

    go

--- app/Models/HemogramaCompleto.php
+++ app/Models/HemogramaCompleto.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class HemogramaCompleto extends Model
 {
     use HasFactory;
-    protected $fillable = ['id'];
+    protected $fillable = [
+        'globulosRojos',
+    ];
+    protected $table = 'hemograma_completos';
     public function analisis()
     {
         return $this->belongsTo(Analisis::class, 'idAnalisis');
--- resources/views/hemograma/edit.blade.php
+++ resources/views/hemograma/edit.blade.php
@@ -4,12 +4,49 @@
 
 @stop
 @section('content')
-    <form action="" method="POST" class="needs-validation" novalidate>
+    <form action="{{ route('hemograma.update', $hemograma->id) }}" method="POST">
         @csrf
-        <label for="validationCustom01">Nro de Hemograma: {{ $hemogramaCompleto->id }}</label>
-
+        @method('PUT')
 
         <label class="mt-2">Hemograma</label>
+        <div class="form-row">
+            <div class="col-md-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroupPrepend">Nro Orden</span>
+                    </div>
+                    <input type="text" class="form-control" id="validationCustom01" placeholder="First name"
+                        value="{{ $hemograma->analisis->orden->nroOrden}}" readonly>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroupPrepend">Paciente</span>
+                    </div>
+                    <input type="text" class="form-control" id="validationCustom01" placeholder="First name"
+                        value="{{ $hemograma->analisis->orden->paciente->nombre}}" readonly>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroupPrepend">Bioquímico</span>
+                    </div>
+                    <select class="custom-select" id="idbioquimico" name="idbioquimico" required>
+                        <option value="{{$hemograma->analisis->bioquimico->id}}">{{$hemograma->analisis->bioquimico->nombre}}</option>
+                        @foreach ($bioquimico as $b)
+                            @if ($hemograma->analisis->bioquimico->id!=$b->id)
+                            <option value="{{$b->id}}">{{$b->nombre}}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </div>
+
+
+
         <div class="form-row">
             <div class="col">
                 <label class="mt-1 text-danger">Serie Roja</label>
@@ -17,8 +54,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Globulos Rojos</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="" name="globulosrojo" id="globulosrojo"
-                        aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="globulosrojos" id="globulosrojos" aria-describedby="inputGroupPrepend" value="{{ $hemograma->globulosRojos }}">
 
                 </div>
             </div>
@@ -28,7 +64,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Globulos Blancos</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="globulosblanco" id="globulosblanco" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="globulosblanco" id="globulosblanco"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -38,7 +75,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Hematocrito</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="" name="hematocrito" id="hematocrito" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="hematocrito" id="hematocrito"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
             <div class="col">
@@ -46,7 +84,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Promielocitos</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="" name="promielocito" id="promielocito" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="promielocito" id="promielocito"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -56,7 +95,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Hemoglobina</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="" name="hemoglobina" id="hemoglobina" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="hemoglobina" id="hemoglobina"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
             <div class="col">
@@ -64,7 +104,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Mielocitos</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="" name="mielocito" id="mielocito" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="mielocito" id="mielocito"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -74,7 +115,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">V.C.M</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="" name="VCM" id="VCM" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="VCM" id="VCM"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
             <div class="col">
@@ -82,7 +124,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Metamielocitos</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="" name="metamielocitos" id="metamielocitos" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="metamielocitos" id="metamielocitos"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -92,7 +135,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">H.C.M</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="" name="HCM" id="HCM" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="HCM" id="HCM"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
             <div class="col">
@@ -100,7 +144,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Cayados</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="cayados" id=cayados"" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="cayados" id=cayados""
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -110,7 +155,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">C.H.C.M</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="CHCM" id="CHCM" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="CHCM" id="CHCM"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
             <div class="col">
@@ -118,7 +164,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Segmentados</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="segmentados" id="segmentados" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="segmentados" id="segmentados"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -128,7 +175,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">VSG 1 a h</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="VSG" id="VSG" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="VSG" id="VSG"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
             <div class="col">
@@ -136,7 +184,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Linfocitos</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="linfocitos" id="linfocitos" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="linfocitos" id="linfocitos"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -146,7 +195,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Plaquetas</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="plaquetas" id="plaquetas" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="plaquetas" id="plaquetas"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
             <div class="col">
@@ -154,7 +204,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Monocitos</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="monocitos" id="monocitos" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="monocitos" id="monocitos"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -164,7 +215,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Recuento</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="recuento" id="recuento" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="recuento" id="recuento"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
             <div class="col">
@@ -172,7 +224,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Eosinofilos</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="eosinofilos" id="eosinofilos" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="eosinofilos" id="eosinofilos"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -188,7 +241,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Blastos</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="blastos" id="blastos" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="blastos" id="blastos"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
@@ -196,7 +250,7 @@
         <div class="form-row">
             <div class="col">
                 <div class="input-group">
-                    <textarea class="form-control"  name="descripcion" id="descripcion" placeholder=""></textarea>
+                    <textarea class="form-control" name="descripcion" id="descripcion" placeholder=""></textarea>
                 </div>
             </div>
         </div>
@@ -206,7 +260,8 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Grupo Sanguineo</span>
                     </div>
-                    <input type="text" class="form-control"  name="gruposanguineo" id="gruposanguineo" placeholder="" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" name="gruposanguineo" id="gruposanguineo" placeholder=""
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
             <div class="col">
@@ -214,24 +269,12 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroupPrepend">Factor Rh</span>
                     </div>
-                    <input type="text" class="form-control" placeholder=""  name="factorrh" id="factorrh" aria-describedby="inputGroupPrepend">
+                    <input type="text" class="form-control" placeholder="" name="factorrh" id="factorrh"
+                        aria-describedby="inputGroupPrepend">
                 </div>
             </div>
         </div>
 
-
-
-        <div class="form-group">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                <label class="form-check-label" for="invalidCheck">
-                    Agree to terms and conditions
-                </label>
-                <div class="invalid-feedback">
-                    You must agree before submitting.
-                </div>
-            </div>
-        </div>
-        <button class="btn btn-primary" type="submit">Submit form</button>
+        <button class="btn btn-primary mt-3 mb-2" type="submit">Submit form</button>
     </form>
 @endsection
